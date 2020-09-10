4b9b7ac23c712ff54f24da6b81cbb47333ae98f1
SoloJette <solemansay@gmail.com>
2020-09-09 20:21:18 -0700

    fixed a few problems

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burger_name) VALUES ('Owens');
